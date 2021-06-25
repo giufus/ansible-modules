421b2f38797e16f516b2648350c44aa2c240a576
giufus <giufus@gmail.com>
2021-06-25 15:06:53 +0200

    update: added ex handling

--- list_modules.py
+++ list_modules.py
@@ -6,15 +6,17 @@ def print_modules(keyword):
 
     page = requests.get("https://docs.ansible.com/ansible/latest/collections/index_module.html")
     soup = bs4.BeautifulSoup(page.content, 'html.parser')
-    modules = soup.find_all(id='index-of-all-modules')[0].find_all('li')
-    for module in modules:
-        description = module.text
+    try:
+        modules = soup.find_all(id='index-of-all-modules')[0].find_all('li')
+        for module in modules:
+            description = module.text
 
-        if(keyword in description):
-            print(description)
-            print('\t', 'https://docs.ansible.com/ansible/latest/collections/' + module.find_all('a')[0]['href'])
-            print()
-
+            if(keyword in description):
+                print(description)
+                print('\t', 'https://docs.ansible.com/ansible/latest/collections/' + module.find_all('a')[0]['href'])
+                print()
+    except Exception as ex:
+        print(f'Unable to get module catalog: {ex}')
 
 
 
